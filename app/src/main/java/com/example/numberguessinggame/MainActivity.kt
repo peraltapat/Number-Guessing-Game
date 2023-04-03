c634ef86321788f3386b06fb92cefe71b6376a32
patrick <patrickperaltapup@gmail.com>
2023-04-03 15:03:29 +0800

    Final version of the Application

--- app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -5,21 +5,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        assignNumbersInsideButtons()
+
         btnLeft.setOnClickListener {
-            val leftNum = btnLeft.text.toString().toInt()
-            val rightNum = btnRight.text.toString().toInt()
-            if(leftNum > rightNum){
-                backgroundView.setBackgroundColor(Color.GREEN)
-                Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
-            }else
-                backgroundView.setBackgroundColor(Color.RED)
-                Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show()
+            checkAnswer(isLeftButtonSelected = true)
+            assignNumbersInsideButtons()
         }
+
+        btnRight.setOnClickListener {
+            checkAnswer(isLeftButtonSelected = false)
+            assignNumbersInsideButtons()
+        }
+    }
+
+    private fun checkAnswer(isLeftButtonSelected: Boolean){
+        val leftNum = btnLeft.text.toString().toInt()
+        val rightNum = btnRight.text.toString().toInt()
+        val isAnswerCorrect = if(isLeftButtonSelected) leftNum > rightNum else rightNum > leftNum
+        if(isAnswerCorrect){
+            backgroundView.setBackgroundColor(Color.GREEN)
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+        }else{
+            backgroundView.setBackgroundColor(Color.RED)
+            Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun assignNumbersInsideButtons() {
+        val r = Random()
+        val leftNum = r.nextInt(50) //bound is the maximum number that you want to hit
+        var rightNum = leftNum
+        while (rightNum == leftNum){
+            rightNum = r.nextInt(50)
+        }
+        btnLeft.text = leftNum.toString()
+        btnRight.text = rightNum.toString()
     }
 }
